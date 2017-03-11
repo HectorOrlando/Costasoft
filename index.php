b963e61daed7d27e9e889f8fbb7e5b121dc440c6
HectorOrlando <14orgui@gmail.com>
2017-03-11 16:45:53 +0100

    2º comentario (Mi nombre) desde mi casa Costasoft.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Esto es una prueba desde mi casa";
+        echo "Hola Mundo soy Hector Orlando Guavita Rojas";
         ?>
     </body>
 </html>
