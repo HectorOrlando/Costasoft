125cd6585bfea28dc27f56bfd56fcaf6427dbef9
HectorOrlando <14orgui@gmail.com>
2017-03-11 17:02:26 +0100

    D. Revierte los cambios que has hecho en el apartado B y haz una captura  de pantalla. Costasoft.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Hola Mundo soy Hector Orlando Guavita Rojas";
+        echo "Hola Mundo";
         ?>
     </body>
 </html>
